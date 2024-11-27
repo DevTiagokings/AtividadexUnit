3ee365c13b149813edb64372314378cd3c5db7c8
Tiago Gomes do Nascimento <Gomes.gt001@gmail.com>
2024-11-27 13:38:44 -0300

    Test com Xunit

--- atividade_imc_xunit/obj/Debug/net8.0/atividade_imc_xunit.AssemblyInfo.cs
+++ atividade_imc_xunit/obj/Debug/net8.0/atividade_imc_xunit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atividade_imc_xunit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3bb7de9edbdd4da9fd6f54c37cac7e5cdf8557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eed23dbd53e5b46d74079585387752a872ca6793")]
 [assembly: System.Reflection.AssemblyProductAttribute("atividade_imc_xunit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atividade_imc_xunit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
